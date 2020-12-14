1e3c1b713c8cc4e1e3761128a5c9cbe721f9499d
Peter Murray <681306+peter-murray@users.noreply.github.com>
2020-12-14 14:17:01 +0000

    Fixing dependabot branch names breaking docker tags

--- .github/workflows/scripts/build_parameters.js
+++ .github/workflows/scripts/build_parameters.js
@@ -13,9 +13,8 @@ class BuildParameters {
     }
 
     injectBuildParameters() {
-        const core = this.core
-            , context = this.context
-        ;
+        const core = this.core,
+            context = this.context;
 
         const shortSha = context.sha.substring(0, 8);
         core.setOutput('github_short_sha', shortSha);
@@ -23,9 +22,8 @@ class BuildParameters {
         core.setOutput('github_ref', context.ref);
         console.log(`GitHub Ref: ${context.ref}`);
 
-        const branchNameMatch = BRANCH_REGEX.exec(context.ref)
-            , branchName = branchNameMatch ? branchNameMatch[1] : ''
-        ;
+        const branchNameMatch = BRANCH_REGEX.exec(context.ref),
+            branchName = branchNameMatch ? branchNameMatch[1] : '';
         core.setOutput('github_ref_branch_name', branchName);
         console.log(`Resolved GitHub Branch Name: ${branchName}`);
 
@@ -34,12 +32,14 @@ class BuildParameters {
         core.setOutput('github_organization_name', context.repo.owner); //Not used currently
         core.setOutput('github_repository_name', context.repo.repo);
         core.setOutput('github_repository', process.env.GITHUB_REPOSITORY);
-        
-        core.setOutput('container_name', `${context.repo.owner}/${context.repo.repo}`)
     }
 
     mavenVersionOutputs(branchName, shortSha) {
         const core = this.core;
+
+        // Dependabot branches are not ideal as they blow up the version number and potentially contain characters we might find undesirable for versioning
+        // As such we need to capture and santize these values to ensure short, meaningful and the non-breaking version numbers and container tags.
+        const cleanBranchName = this.cleanBranchName(branchName);
 
         // Maven CD version number for the project is composed of '${revision}${changelist}${sha1}'
         //
@@ -48,12 +48,22 @@ class BuildParameters {
         //
         // We also need to append '-SNAPSHOT' to the end of the version number unless we are a release build, which
         // in the context of the Maven POM here, is the main branch (as that automatically deploys to prod).
-        if (branchName === 'main') {
+        if (branchName === 'main' || branchName === 'master') {
             core.setOutput('maven_changelist', '');
             core.setOutput('maven_sha1', `-${shortSha}`);
         } else {
-            core.setOutput('maven_changelist', `-${branchName}`);
+            core.setOutput('maven_changelist', `-${cleanBranchName}`);
             core.setOutput('maven_sha1', `-${shortSha}-SNAPSHOT`);
+        }
+    }
+
+    cleanBranchName(name) {
+        const matched = /dependabot\/(.*?)\//.exec(name)
+
+        if (matched) {
+            return `dbot-${matched[1]}`
+        } else {
+            return name;
         }
     }
 }
